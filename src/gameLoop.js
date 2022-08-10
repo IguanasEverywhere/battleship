15571e0efad7957ddd7396883fe32d422b1645a2
Scott Schwab <schwabpiano@gmail.com>
2022-08-09 21:42:08 -0500

    Re render board for updated ship placement

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -1,17 +1,22 @@
-import { playerBoard, playerGameboard, computerGameboard } from "./gameboard";
+import { playerGameboard, computerGameboard } from "./gameboard";
+import { renderBoard } from ".";
 import { battleShip, cruiser } from "./shipFactory";
 
-const startGame = () => {
+const placeShips = () => {
     playerGameboard.placeShipVertically(battleShip, 0, 0);
     playerGameboard.placeShipVertically(cruiser, 0, 2);
     playerGameboard.placedShipArray.forEach(spot => {
         let occupiedSpace = playerGameboard.coordinatesArr.find(place => place.xCoord === spot.xCoord && place.yCoord === spot.yCoord);
         if (occupiedSpace) {
             occupiedSpace.spaceOccupied = true;
+            
         }
     });
 
+    let boardsContainer = document.getElementsByClassName('boards-container')[0];
+    boardsContainer.remove();
 
+    renderBoard();
 }
 
-export { startGame }
+export { placeShips }
--- src/index.js
+++ src/index.js
@@ -1,43 +1,54 @@
 import './style.css';
 import { playerGameboard, computerGameboard } from './gameboard';
-import { startGame } from './gameLoop';
+import { placeShips } from './gameLoop';
 
-startGame();
+const renderBoard = () => {
 
-const boardsContainer = document.createElement("div");
-boardsContainer.classList.add("boards-container");
-document.body.appendChild(boardsContainer);
+  const boardsContainer = document.createElement("div");
+  boardsContainer.classList.add("boards-container");
+  document.body.appendChild(boardsContainer);
 
+  const playerBoard = document.createElement("div");
+  playerBoard.classList.add("player-board");
+  boardsContainer.appendChild(playerBoard);
 
-const playerBoard = document.createElement("div");
-playerBoard.classList.add("player-board");
-boardsContainer.appendChild(playerBoard);
+  const computerBoard = document.createElement("div");
+  computerBoard.classList.add("player-board");
+  boardsContainer.appendChild(computerBoard);
 
-const computerBoard = document.createElement("div");
-computerBoard.classList.add("player-board");
-boardsContainer.appendChild(computerBoard);
-
-playerGameboard.coordinatesArr.forEach(coordinatePair => {
-  let space = document.createElement("div");
-  let spaceCoords = { coordinatePair };
-  space.classList.add("board-space");
-  if (coordinatePair.spaceOccupied) {
-    space.style.backgroundColor = "gray";
-  }
-  playerBoard.appendChild(space);
-  space.addEventListener('click', () => {
-    console.log(spaceCoords);
+  playerGameboard.coordinatesArr.forEach(coordObj => {
+    let space = document.createElement("div");
+    space.setAttribute('isOccupied', coordObj.spaceOccupied);
+    space.classList.add("board-space");
+    if (space.getAttribute("isOccupied") === 'true') {
+      space.style.backgroundColor = "gray"
+    }
+    playerBoard.appendChild(space);
   });
-});
 
-computerGameboard.coordinatesArr.forEach(coordinatePair => {
-  let space = document.createElement("div");
-  let spaceCoords = { coordinatePair };
-  space.classList.add("board-space");
-  computerBoard.appendChild(space);
-  space.addEventListener('click', () => {
-    console.log(spaceCoords);
+  computerGameboard.coordinatesArr.forEach(coordinatePair => {
+    let space = document.createElement("div");
+    let spaceCoords = { coordinatePair };
+    space.classList.add("board-space");
+    computerBoard.appendChild(space);
+    space.addEventListener('click', () => {
+      space.textContent = "X";
+    });
   });
-});
 
-export { playerBoard }
+  const placeShipsBtn = document.createElement("button");
+  placeShipsBtn.textContent = "Place Ships";
+  boardsContainer.appendChild(placeShipsBtn);
+
+  placeShipsBtn.addEventListener('click', () => {
+    placeShips();
+  });
+}
+
+renderBoard();
+
+
+
+
+export { renderBoard }
+
